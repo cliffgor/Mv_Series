09934c6c328eb11dc1ea31fca79f1569d9a2d13a
Cliff Gor <clierozigy@gmail.com>
2019-09-20 15:46:43 +0300

    added a timer for the slider

--- app/src/main/java/com/example/mv_series/MainActivity.java
+++ app/src/main/java/com/example/mv_series/MainActivity.java
@@ -5,13 +5,19 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+
+import com.google.android.material.tabs.TabLayout;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     private List<Slide> lstSlides ;
     private ViewPager sliderpager;
+    private TabLayout indicator;
 
 
 
@@ -22,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         sliderpager = findViewById(R.id.slider_pager);
+        indicator = findViewById(R.id.indicator);
 
 
 
@@ -36,7 +43,39 @@ public class MainActivity extends AppCompatActivity {
 
         sliderpager.setAdapter(adapter);
 
+//        SETUP TIMER
 
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new MainActivity.SliderTimer(), 4000,6000);
+
+
+
+        indicator.setupWithViewPager(sliderpager, true);
+
+
+    }
+
+    class SliderTimer extends TimerTask {
+
+
+
+        @Override
+        public void run(){
+
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                        if (sliderpager.getCurrentItem()<lstSlides.size()-1) {
+                            sliderpager.setCurrentItem(sliderpager.getCurrentItem()+1);
+                        }
+                        else
+                            sliderpager.setCurrentItem(0);
+                }
+            });
+
+
+
+        }
     }
 
 
